b84c13884cd7e9f2bddbcf1a3bdf95e443525080
Farman Ali Ujjan <farman20ali@gmail.com>
2025-12-06 19:38:28 +0500

    updating version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="kport",
-    version="1.1.0",
+    version="2.1.0",
     author="Farman Ali",
     author_email="farman20ali@gmail.com",
     description="A cross-platform command-line tool to inspect and kill processes using specific ports",
